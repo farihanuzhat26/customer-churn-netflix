aaa1107267af6d495c352e2f1bf4b681076cee74
farihanuzhat26 <farihanzht@gmail.com>
2025-07-31 00:43:03 -0400

    removing git stuff app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/farihanuzhat26/customer-churn-netflix.git
-git push -u origin main
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
